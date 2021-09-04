b5cfe9bd4d36c33e367e3a67db5ac269a346e8b5
João Gabriel <joao.gabrielfjob@gmail.com>
2021-09-04 07:57:17 -0300

    vid #7 - view engines with ejs

--- the-net-ninja/app.js
+++ the-net-ninja/app.js
@@ -2,22 +2,27 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (request, response) => {
-  //response.send('<p> home page </p>');
-  response.sendFile('./views/index.html', { root: __dirname });
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+
+  response.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (request, response) => {
-  //response.send('<p> about page </p>');
-  response.sendFile('./views/about.html', { root: __dirname });
+  response.render('about', { title: 'About' });
 });
 
-app.get('/about-us', (request, response) => {
-  response.redirect('/about');
-});
+app.get('/blogs/create', (request, response) => {
+  response.render('create', { title: 'Create a new blog' });
+})
 
 app.use((request, response) => {
-  response.status(404).sendFile('./views/404.html', { root: __dirname });
+  response.status(404).render('404', { title: '404' });
 });
