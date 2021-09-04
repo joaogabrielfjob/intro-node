ff9eeebe026f0f2c13d317e4fa27ba55f977f118
João Gabriel <joao.gabrielfjob@gmail.com>
2021-09-04 08:57:05 -0300

    vid #9 - mongo db with mongoose

--- the-net-ninja/app.js
+++ the-net-ninja/app.js
@@ -1,22 +1,54 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 const app = express();
+const dbURI = 'mongodb+srv://sample-user:teste123@intro-node.fyzdp.mongodb.net/intro-node?retryWrites=true&w=majority';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((error) => console.error(error));
 
 app.set('view engine', 'ejs');
-app.listen(3000);
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
-app.get('/', (request, response) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
+app.get('/add-blog', (request, response) => {
+  const blog = new Blog({
+    title: 'new blog 2',
+    snippet: 'about my new blog 2',
+    body: 'more about my new blog 2',
+  });
 
-  response.render('index', { title: 'Home', blogs });
+  blog.save()
+    .then((result) => response.send(result))
+    .catch((error) => console.error(error));
+});
+
+app.get('/all-blogs', (request, response) => {
+  Blog.find()
+    .then((result) => response.send(result))
+    .catch((error) => console.error(error));
+});
+
+app.get('/single-blog', (request, response) => {
+  Blog.findById('61335c920cabe9ffca0dca2d')
+    .then((result) => response.send(result))
+    .catch((error) => console.error(error));
+})
+
+app.get('/', (request, response) => {
+  response.redirect('/blogs')
+});
+
+app.get('/blogs', (request, response) => {
+  Blog.find().sort({ createdAt: -1 })
+    .then((result) => {
+      response.render('index', { title: 'All blogs', blogs: result });
+    })
+    .catch((error) => console.error(error));
 });
 
 app.get('/about', (request, response) => {
