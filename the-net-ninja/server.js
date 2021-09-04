50151bcd8345e147cc70b9203aac46c8a3a7a24f
João Gabriel <joao.gabrielfjob@gmail.com>
2021-09-03 22:10:08 -0300

    vid #4 - request, response, returning html, basic routing, status code and redirects

--- the-net-ninja/server.js
+++ the-net-ninja/server.js
@@ -1,7 +1,40 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((request, response) => {
-  console.log('request made');
+  let path = './views/';
+
+  switch(request.url) {
+    case '/': 
+      path += 'index.html';
+      response.statusCode = 200;
+      break;
+    
+    case '/about':
+      path += 'about.html';
+      response.statusCode = 200;
+      break;
+
+    case '/about-me':
+      response.statusCode = 301;
+      response.setHeader('Location', '/about');
+      response.end();
+      break;
+    
+    default: 
+      path += '404.html';
+      response.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (error, data) => {
+    if (error) {
+      console.error(error);
+    } else {
+      response.setHeader('Content-Type', 'text/html');
+      response.end(data);
+    }
+  });
 });
 
 server.listen(3000, 'localhost', () => {
